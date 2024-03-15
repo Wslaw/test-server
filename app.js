d3bbfbdfe0eb291a419c895b52950cdb8402f528
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-03-15 15:38:46 +0200

    2

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 import express from "express";
 
 import { contacts } from "./contacts.js";
+
+import moment from "moment";
+
+import fs from "fs/promises"
+import cors from "cors"
+
 const app = express();
 
-app.get('/', (req, res) => {
-     console.log(req.url);
-     console.log(req.method);
-    res.send('<h2>Home page</h2>')
+// ********************************
+// const corsMiddleware = cors();
+// app.use(corsMiddleware);
+app.use(cors());
+// middleware
+/*
+app.use((req, res, next) => {
+    console.log("First middleware");
+    next();
 })
+app.use((req, res, next) => {
+    console.log("Second middleware");
+    next();
+})
+*/
+// ***************************************************************
+// app.use(async(req, res, next) => {
+//     const { method, url } = req;
+//     const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+//     await fs.appendFile("./server.log", `\n${method} ${url} ${date}`);
+//     next();
+// })
 
-app.get('/contacts', (req, res) => {
+app.get('/products', async (req, res) => {
+    
+    res.json([])
+});
+
+app.get('/contacts', async (req, res) => {
     
     // res.send(null)
     // res.json(null)
     // res.send(contacts)
     res.json(contacts)
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Not found"
+    })
 })
 
 app.listen(3001,()=>console.log("--Serveris running--"))
